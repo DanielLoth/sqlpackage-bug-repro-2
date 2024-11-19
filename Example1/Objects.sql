921b57275b3ad92382a41b26139a5fd3b05d1503
Daniel Loth <650630+DanielLoth@users.noreply.github.com>
2024-11-20 10:25:00 +1100

    Initial commit

---
+++ Example1/Objects.sql
@@ -0,0 +1,28 @@
+﻿/*
+================================================================================
+GENERATED STATEMENTS EACH DEPLOYMENT
+================================================================================
+
+Nil - works as expected.
+*/
+
+create role MyRole;
+go
+
+create user MyUser without login;
+go
+
+create schema MySchema; /* Implicit authorization to dbo */
+go
+
+create type MySchema.MyType as table (Id int);
+go
+
+grant execute on type::MySchema.MyType to MyRole;
+go
+
+create type MySchema.MyTypeGrantWithAuthorizer as table (Id int);
+go
+
+grant execute on type::MySchema.MyTypeGrantWithAuthorizer to MyRole as dbo;
+go
---
+++ Example2/Objects.sql
@@ -0,0 +1,40 @@
+﻿/*
+================================================================================
+GENERATED STATEMENTS EACH DEPLOYMENT
+================================================================================
+
+REVOKE EXECUTE
+    ON TYPE::[MySchema].[MyTypeGrantWithAuthorizer] TO [MyRole] CASCADE;
+GO
+GRANT EXECUTE
+    ON TYPE::[MySchema].[MyTypeGrantWithAuthorizer] TO [MyRole]
+    AS [MyUser];
+GO
+GRANT EXECUTE
+    ON TYPE::[MySchema].[MyType] TO [MyRole];
+*/
+
+create role MyRole;
+go
+
+create user MyUser without login;
+go
+
+create schema MySchema authorization MyUser;
+go
+
+create type MySchema.MyType as table (Id int);
+go
+
+grant execute on type::MySchema.MyType to MyRole;
+go
+
+create type MySchema.MyTypeGrantWithAuthorizer as table (Id int);
+go
+
+/*
+This statement results in a REVOKE EXECUTE ... CASCADE followed by a
+GRANT EXECUTE ... AS [MyUser] statement.
+*/
+grant execute on type::MySchema.MyTypeGrantWithAuthorizer to MyRole as MyUser;
+go
